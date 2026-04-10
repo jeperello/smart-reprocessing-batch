d30dcc1da9587523fa3452987549ceddd6f417a8
jeperello <jorgeperello2020@gmail.com>
2026-04-10 17:30:14 -0300

    chore: initial project structure with github actions

---
+++ src/main/java/com/example/smart_reprocessing_batch/SmartReprocessingBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_reprocessing_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReprocessingBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartReprocessingBatchApplication.class, args);
+	}
+
+}
